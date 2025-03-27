e060833b6f02b6ba1ed52fcc0e4bd1d83ab87467
kalyan2003 <pavan.chimmiri2003@gmail.com>
2025-03-27 17:18:53 +0530

    Implemented the usecase-3

--- snake_and_ladder/snake_and_laddergame.py
+++ snake_and_ladder/snake_and_laddergame.py
@@ -10,3 +10,22 @@ class SnakeLadder:
     @staticmethod
     def roll_dice():
         return random.randint(1, 6)
+
+    def play_turn(self):
+        dice_value = SnakeLadder.roll_dice()  
+        option = random.choice(["No Play", "Ladder", "Snake"])
+
+        if option == "No Play":
+            pass
+        elif option == "Ladder":
+            self.position += dice_value
+        elif option == "Snake":
+            self.position -= dice_value
+            if self.position < 0:
+                self.position = 0
+
+        print(f"Dice rolled: {dice_value}, Option: {option}, New Position: {self.position}")
+
+
+game = SnakeLadder()
+game.play_turn()
